1d9e5c79dc359922201cc4bd7bc72bd70bd6ebb8
Raphael Pinto <60246144+Raff29@users.noreply.github.com>
2024-02-03 11:13:25 -0400

    changed hash method, and salt length

--- server/web/project/models.py
+++ server/web/project/models.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100), nullable=False)
     
     def set_password(self, password):
-        self.password = generate_password_hash(password, method='sha256')
+        self.password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=20)
         
     def check_password(self, password):
         return check_password_hash(self.password, password)
