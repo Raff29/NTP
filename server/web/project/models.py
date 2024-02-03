5fede8e8726b2a52334ca1d1f5806babe99a5dd1
Raphael Pinto <60246144+Raff29@users.noreply.github.com>
2024-02-02 22:50:18 -0400

    added schema for instructions log table

--- server/web/project/models.py
+++ server/web/project/models.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 from . import db
 
 class User(UserMixin, db.Model):
@@ -16,3 +17,13 @@ class User(UserMixin, db.Model):
       
     def __repr__(self):
         return f"<User {self.email}>"
+
+    class InstructionLog(db.Model):
+        __tablename__ = 'instruction_logs'
+        id = db.Column(db.Integer, primary_key=True)
+        instructions = db.Column(db.String(100), nullable=False)
+        user_email = db.Column(db.String(100), db.ForeignKey('users.email'), nullable=False)
+        timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+        def __repr__(self):
+            return f"<InstructionLog {self.id}>"
