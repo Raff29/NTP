03d6464351565375916c8b12d5c18175e9bee34a
Raphael Pinto <60246144+Raff29@users.noreply.github.com>
2024-03-04 14:46:59 -0400

    cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignInForm from "./components/SignInForm";
 import Layout from "./components/Layout";
 import { ThemeProvider } from "@emotion/react";
@@ -20,34 +15,34 @@ function App() {
       <Router>
         {/* <ThemeProvider theme={theme}> */}
         <Layout>
-        <Routes>
-  <Route path="/" />
-  <Route
-    path="/register"
-    element={
-      <PublicRoutes>
-        <SignUpForm email="" password="" confirm_password="" />
-      </PublicRoutes>
-    }
-  />
-  <Route
-    path="/login"
-    element={
-      <PublicRoutes>
-        <SignInForm email="" password="" />
-      </PublicRoutes>
-    }
-  />
-  <Route
-    path="/dashboard"
-    element={
-      <PrivateRoutes>
-        <Dashboard />
-      </PrivateRoutes>
-    }
-  />
-</Routes>
-          </Layout>
+          <Routes>
+            <Route path="/" />
+            <Route
+              path="/register"
+              element={
+                <PublicRoutes>
+                  <SignUpForm email="" password="" confirm_password="" />
+                </PublicRoutes>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PublicRoutes>
+                  <SignInForm email="" password="" />
+                </PublicRoutes>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoutes>
+                  <Dashboard />
+                </PrivateRoutes>
+              }
+            />
+          </Routes>
+        </Layout>
         {/* </ThemeProvider> */}
       </Router>
     </AuthProvider>
