62be5b67962b1a812f4420223b38116e76e3bc02
Raphael Pinto <60246144+Raff29@users.noreply.github.com>
2024-03-30 17:22:57 -0300

    added context

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import { Container, Paper, Typography } from '@mui/material';
 import FileViewer from './FileViewer';
+import FileUploader from "./FileUploader";
 
 const Dashboard: FC = () => {
   return (
     <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '85vh', padding: '8', backgroundColor: 'gray.100' }}>
       <Paper className="w-full max-w-2xl p-8 text-center">
         <Typography variant='h4' gutterBottom className="mb-8">Music List</Typography>
+        <FileUploader />
         <FileViewer />
       </Paper>
     </Container>
