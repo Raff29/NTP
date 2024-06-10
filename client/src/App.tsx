63ba000c13174c421e035eb6e6b319f3eef835aa
Raphael Pinto <60246144+Raff29@users.noreply.github.com>
2024-06-09 21:55:38 -0300

    formatting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,80 +8,80 @@ import PublicRoutes from "./utils/PublicRoutes";
 import { AuthProvider } from "./context/AuthContext";
 import { FilesProvider } from "./context/FileContext";
 import LandingPage from "./components/Landing";
-import { createTheme,ThemeProvider } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useEffect } from "react";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#2D3748', 
+      main: "#2D3748",
     },
     secondary: {
-      main: '#38B2AC',
+      main: "#38B2AC",
     },
   },
   typography: {
     h2: {
-      fontFamily: 'Roboto, sans-serif',
+      fontFamily: "Roboto, sans-serif",
       fontWeight: 700,
     },
     h5: {
-      fontFamily: 'Roboto, sans-serif',
+      fontFamily: "Roboto, sans-serif",
       fontWeight: 400,
     },
     body1: {
-      fontFamily: 'Roboto, sans-serif',
+      fontFamily: "Roboto, sans-serif",
       fontWeight: 300,
     },
   },
 });
 
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <AuthProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <PublicRoutes>
-                  <LandingPage/>
-                </PublicRoutes>
-              }
-            />
-            <Route
-              path="/register"
-              element={
-                <PublicRoutes>
-                  <SignUpForm email="" password="" confirm_password="" />
-                </PublicRoutes>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <PublicRoutes>
-                  <SignInForm email="" password="" />
-                </PublicRoutes>
-              }
-            />
-            <Route
-              path="/dashboard"
-              element={
-                <PrivateRoutes>
-                  <FilesProvider>
-                    <Dashboard />
-                  </FilesProvider>
-                </PrivateRoutes>
-              }
-            />
-          </Routes>
-        </Layout>
-        {/* </ThemeProvider> */}
-      </Router>
-    </AuthProvider>
+      <AuthProvider>
+        <Router>
+          <Layout>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <PublicRoutes>
+                    <LandingPage />
+                  </PublicRoutes>
+                }
+              />
+              <Route
+                path="/register"
+                element={
+                  <PublicRoutes>
+                    <SignUpForm email="" password="" confirm_password="" />
+                  </PublicRoutes>
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <PublicRoutes>
+                    <SignInForm email="" password="" />
+                  </PublicRoutes>
+                }
+              />
+              <Route
+                path="/dashboard"
+                element={
+                  <PrivateRoutes>
+                    <FilesProvider>
+                      <Dashboard />
+                    </FilesProvider>
+                  </PrivateRoutes>
+                }
+              />
+            </Routes>
+          </Layout>
+          {/* </ThemeProvider> */}
+        </Router>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
