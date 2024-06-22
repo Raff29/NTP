c306b853313c0025821dfb6ee1404410cdb6862c
Raphael Pinto <60246144+Raff29@users.noreply.github.com>
2024-06-22 10:42:49 -0300

    fixed issue with password hashing

--- server/web/project/routes/profile.py
+++ server/web/project/routes/profile.py
@@ -5,28 +5,31 @@ from .. import db
 
 profile = Blueprint('profile', __name__)
 
+
 @profile.route('/profile', methods=['GET'])
 @login_required
 def get_profile():
-  user = User.query.filter_by(id=current_user.id).first()
-  if not user:
-    return jsonify({'error': 'Profile not found'}), 404
-  
-  return jsonify(user.to_dict()), 200
+    user = User.query.filter_by(id=current_user.id).first()
+    if not user:
+        return jsonify({'error': 'Profile not found'}), 404
+
+    return jsonify(user.to_dict()), 200
+
 
 @profile.route('/profile/update/<int:id>', methods=['PATCH'])
 @login_required
 def update_profile(id):
-  data = request.get_json()
-  
-  user = User.query.get(id)
-  if not user:
-    return jsonify({'error': 'Profile not found'}), 404
+    data = request.get_json()
 
-  user.name = data.get('name', user.name)
-  user.email = data.get('email', user.email)
-  user.password = data.get('password', user.password)
-  
-  db.session.commit()
+    user = User.query.get(id)
+    if not user:
+        return jsonify({'error': 'Profile not found'}), 404
 
-  return jsonify(user.to_dict()), 200
+    user.name = data.get('name', user.name)
+    user.email = data.get('email', user.email)
+    if 'password' in data:
+        user.set_password(data['password'])
+
+    db.session.commit()
+
+    return jsonify(user.to_dict()), 200
